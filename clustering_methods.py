88a4cc1c393607d2b0731186f54560782150cf2b
xavier <xavier.favory@gmail.com>
2017-01-27 17:34:23 +0100

    Added a clustering result

--- clustering_methods.py
+++ clustering_methods.py
@@ -13,6 +13,7 @@ import operator
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.metrics.pairwise import euclidean_distances
 import matplotlib.pyplot as plt
+import json
 
 
 def kmeans(data, n_clusters=5):
@@ -45,7 +46,52 @@ def agglomerative(data):
     Z = linkage(data, 'Ward')
     return Z
 
-class knnGraph():
+class Graph():
+    def __init__(self):
+        pass
+        
+    def get_labels(self):
+        return [self.classes[k] for k in range(len(self.classes.keys()))]
+    
+    def plot(self):
+        nx.draw(self.graph)
+        plt.show()
+    
+    def get_clusters(self):
+        id_to_seq = json.load(open('dissimilarityMatrix/index2Pattern_w_ornament.json', 'rb'))
+        labels = self.get_labels()
+        clusters = []
+        for k in range(max(labels)):
+            clusters.append([id_to_seq[str(s)] for s in range(2022) if labels[s]==k])  
+        return clusters
+
+class weightedGraph(Graph):
+    """
+    Graph based clustering using Louvain aglorithm.
+    
+    Dependencies
+    ----------
+    networkx, community, matplotlib
+    
+    Parameters
+    ----------
+    similarity_matrix : 2D array
+    """
+    
+    def __init__(self, similarity_matrix):
+        self.graph = self.create_weighted_graph(similarity_matrix)
+        self.classes = com.best_partition(self.graph)
+        
+    def create_weighted_graph(self, similarity_matrix):
+        """ Returns a weighted from a similarity matrix - NetworkX module """
+        np.fill_diagonal(similarity_matrix, 0) # for removing the 1 from diagonal
+        g = nx.Graph()
+        g.add_nodes_from(range(len(similarity_matrix)))
+        for idx in range(len(similarity_matrix)):
+            g.add_weighted_edges_from([(idx, i[0], i[1]) for i in zip(range(len(similarity_matrix)), similarity_matrix[idx]) if i[0]!=idx and i[1]!=0])
+        return g     
+
+class knnGraph(Graph):
     """
     Knn-graph based clustering using Louvain aglorithm.
     
@@ -76,29 +122,33 @@ class knnGraph():
         self.graph = self.create_knn_graph(similarity_matrix, k)
         self.classes = com.best_partition(self.graph)
         
-    def get_labels(self):
-        return [self.classes[k] for k in range(len(self.classes.keys()))]
-    
-    def plot(self):
-        nx.draw(self.graph)
-        plt.show()
-        
     def create_knn_graph(self, similarity_matrix, k):
         """ Returns a knn graph from a similarity matrix - NetworkX module """
         np.fill_diagonal(similarity_matrix, 0) # for removing the 1 from diagonal
         g = nx.Graph()
         g.add_nodes_from(range(len(similarity_matrix)))
         for idx in range(len(similarity_matrix)):
-            g.add_edges_from([(idx, i) for i in self.nearest_neighbors(similarity_matrix, idx, k)])
+            g.add_edges_from([(idx, i) for i in self.k_nearest_neighbors(similarity_matrix, idx, k)])
         return g  
     
     @staticmethod
-    def nearest_neighbors(similarity_matrix, idx, k):
+    def k_nearest_neighbors(similarity_matrix, idx, k):
         distances = []
         for x in range(len(similarity_matrix)):
-            distances.append((x,similarity_matrix[idx][x]))
+            distances.append((x, similarity_matrix[idx][x]))
         distances.sort(key=operator.itemgetter(1), reverse=True)
-        return [d[0] for d in distances[0:k]]    
+        if k>0:
+            return [d[0] for d in distances[0:k]]
+        if k==0:
+            # extract the more similar one with the highest similarity
+            r = []
+            i = distances[0][1]
+            idx = 0
+            while (i==distances[0][1]):
+                r.append(distances[idx][0])
+                idx += 1
+                i = distances[idx][1]
+            return r
 
 #X, y = make_blobs(n_samples=10, centers=10, n_features=2, random_state=0)
 #similarity = euclidean_distances(X)
